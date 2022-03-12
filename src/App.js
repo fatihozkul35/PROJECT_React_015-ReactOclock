f7a9a41c16f520cc20cf467236445def69e4e78b
fatihozkul35 <fatihozkul3541@gmail.com>
2022-03-12 18:11:58 +0100

    except styles is done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Oclock from "./components/Oclock";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Oclock />
     </div>
   );
 }
---
+++ src/components/Oclock.jsx
@@ -0,0 +1,47 @@
+import { Container, Row } from "react-bootstrap";
+import "./Oclock.css";
+import { useEffect, useState } from "react";
+const Oclock = () => {
+  const [oclock, setOclock] = useState("");
+  function formatTime(val) {
+    if (val < 10) {
+      return 0;
+    } else {
+      return "";
+    }
+  }
+
+  useEffect(() => {
+    const timer = setInterval(() => tick(), 1000);
+
+    //cleanup
+    return function cleanup() {
+      clearInterval(timer);
+    };
+  });
+  function tick() {
+    const nowTime = new Date();
+    const hour = nowTime.getHours();
+    const minute = nowTime.getMinutes();
+    const second = nowTime.getSeconds();
+
+    return setOclock(
+      formatTime(hour) +
+        hour +
+        ":" +
+        formatTime(minute) +
+        minute +
+        ":" +
+        formatTime(second) +
+        second
+    );
+  }
+  return (
+    <Container className="d-flex flex-column justify-content-center align-items-center">
+      <h1 className="mt-3 mb-5">Oclock Project</h1>
+      <Row>{!oclock ? "" : <div className="oclock">{oclock}</div>}</Row>
+    </Container>
+  );
+};
+
+export default Oclock;
